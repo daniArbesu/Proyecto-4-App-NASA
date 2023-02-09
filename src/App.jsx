7777bc03f42ddf716f94627a93d63444fee7ba43
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-02-09 21:54:44 +0100

    feat: add functionality to the buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 import { useEffect, useState } from 'react';
-import { getRoverPhotos } from './api/rovers';
+import { getRoverPhotos } from './api/nasa';
 import './App.css';
 // import ImageDisplay from './components/Image';
 
@@ -18,13 +18,26 @@ function App() {
   useEffect(() => {
     getRoverPhotos(date)
       .then((data) => setPhoto(data))
-      .catch((err) => console.log(err));
+      .catch((err) => console.error(err));
   }, [date]);
 
   // Crearemos una función que setee la fecha a través de un input en el formato
   // necesario (igual que el formato de today)
   const handleInput = (ev) => {
     setDate(ev.target.value.toLocaleString()); // .toLocaleString() sirve para formatear la fecha
+  };
+
+  const handleShare = () => {
+    if (navigator.share) {
+      navigator
+        .share({
+          title: 'Arbesú Nasa Picture of the Day',
+          text: 'Check out this amazing page',
+          url: 'https://dani-arbesu-nasa.netlify.app/'
+        })
+        .then(() => console.log('Successful share'))
+        .catch((error) => console.log('Error sharing', error));
+    }
   };
 
   return (
@@ -56,7 +69,7 @@ function App() {
           <p>{photo.explanation}</p>
         </div> */}
           <div className="card-actions">
-            <button type="button">
+            <a href="https://github.com/daniArbesu" target="_blank" rel="noreferrer noopener">
               <svg
                 aria-label="Like"
                 color="#262626"
@@ -68,8 +81,8 @@ function App() {
               >
                 <path d="M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z" />
               </svg>
-            </button>
-            <button type="button">
+            </a>
+            <a href="https://github.com/daniArbesu" target="_blank" rel="noreferrer noopener">
               <svg
                 aria-label="Comment"
                 color="#262626"
@@ -87,8 +100,8 @@ function App() {
                   strokeWidth="2"
                 />
               </svg>
-            </button>
-            <button type="button">
+            </a>
+            <button type="button" onClick={handleShare}>
               <svg
                 aria-label="Share Post"
                 color="#262626"
